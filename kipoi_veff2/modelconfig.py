a1aadd1433fc798f852920bf041955c05faacf71
Haimasree <haimasree.de@gmail.com>
2021-03-31 17:36:49 +0200

    Cleanup

--- kipoi_veff2/modelconfig.py
+++ kipoi_veff2/modelconfig.py
@@ -13,25 +13,33 @@ class ModelConfig:
         transform=None,
     ) -> None:
         self.model = model
-        self.model_description = kipoi.get_model_descr(self.model)
         self.scoring_fn = scoring_fn
         self.required_sequence_length = required_sequence_length
         self.transform = transform
+        self.model_description = kipoi.get_model_descr(self.model)
+        self.dataloader = self.model_description.default_dataloader
+
+    def is_sequence_model(self) -> bool:
+        if self.dataloader.defined_as == "kipoiseq.dataloaders.SeqIntervalDl":
+            return True
+        else:
+            return False
 
     def get_model(self) -> str:
         return self.model
 
     def get_transform(self) -> Any:
         if self.transform is None:
-            dataloader = self.model_description.default_dataloader
-            if dataloader.defined_as == "kipoiseq.dataloaders.SeqIntervalDl":
-                dataloader_args = dataloader.default_args
+            if self.is_sequence_model():
+                dataloader_args = self.dataloader.default_args
                 self.transform = ReorderedOneHot(
                     alphabet="ACGT",
                     dtype=dataloader_args["dtype"],
                     alphabet_axis=dataloader_args["alphabet_axis"],
                     dummy_axis=dataloader_args["dummy_axis"],
                 )
+            else:
+                raise IOError("Only supporting sequence based models for now")
         if self.transform is None:
             raise ValueError("Cannot proceed without a transform")
         else:
@@ -39,10 +47,8 @@ class ModelConfig:
 
     def get_required_sequence_length(self) -> Any:
         if self.required_sequence_length is None:
-            self.required_sequence_length = (
-                self.model_description.default_dataloader.default_args.get(
-                    "auto_resize_len", None
-                )
+            self.required_sequence_length = self.dataloader.default_args.get(
+                "auto_resize_len", None
             )
         if self.required_sequence_length is None:
             raise ValueError("Cannot proceed without required sequence length")
