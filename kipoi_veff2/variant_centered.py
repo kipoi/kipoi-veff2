c8f2bfa9ea1676a552fca6536105ff05e358813c
Haimasree <haimasree.de@gmail.com>
2021-04-06 14:38:02 +0200

    Addressing pr reviews

--- kipoi_veff2/variant_centered.py
+++ kipoi_veff2/variant_centered.py
@@ -1,17 +1,79 @@
+from dataclasses import dataclass
+from typing import Any, Callable, List, Iterator
+
 from cyvcf2 import VCF
 import numpy as np
-from typing import List, Iterator
-
 import kipoi
 from kipoiseq.dataclasses import Interval, Variant
 from kipoiseq.extractors import VariantSeqExtractor
 from kipoiseq.transforms import ReorderedOneHot
-from kipoi_veff2.modelconfig import ModelConfig
 
 
-class Diff:
-    def __call__(self, ref_pred: List, alt_pred: List) -> List:
-        return alt_pred - ref_pred
+@dataclass
+class ModelConfig:
+    model: str
+    scoring_fn: Callable[[List, List], List]
+    required_sequence_length: int = None
+    transform: Any = None
+
+    def __post_init__(self):
+        self.model_description = kipoi.get_model_descr(self.model)
+        self.dataloader = self.model_description.default_dataloader
+
+    def is_sequence_model(self) -> bool:
+        if self.dataloader.defined_as == "kipoiseq.dataloaders.SeqIntervalDl":
+            return True
+        else:
+            return False
+
+    def get_model(self) -> str:
+        return self.model
+
+    def get_transform(self) -> Any:
+        if self.transform is None:
+            if self.is_sequence_model():
+                dataloader_args = self.dataloader.default_args
+                self.transform = ReorderedOneHot(
+                    alphabet="ACGT",
+                    dtype=dataloader_args["dtype"],
+                    alphabet_axis=dataloader_args["alphabet_axis"],
+                    dummy_axis=dataloader_args["dummy_axis"],
+                )
+            else:
+                raise IOError("Only supporting sequence based models for now")
+        if self.transform is None:
+            raise ValueError("Cannot proceed without a transform")
+        else:
+            return self.transform
+
+    def get_required_sequence_length(self) -> Any:
+        if self.required_sequence_length is None:
+            self.required_sequence_length = self.dataloader.default_args.get(
+                "auto_resize_len", None
+            )
+        if self.required_sequence_length is None:
+            raise ValueError("Cannot proceed without required sequence length")
+        else:
+            return self.required_sequence_length
+
+    def get_column_labels(self) -> List:
+        targets = self.model_description.schema.targets
+        column_labels = targets.column_labels
+        target_shape = targets.shape[0]
+        if column_labels:
+            if len(column_labels) == target_shape:
+                return [f"{self.model}/{c}" for c in column_labels]
+            else:
+                raise IOError(
+                    "Something wrong with the model description - \
+                        length of column names does not match target shape"
+                )
+        else:
+            return [f"{self.model}/{num+1}" for num in range(target_shape)]
+
+
+def diff(ref_pred: List, alt_pred: List) -> List:
+    return list(alt_pred - ref_pred)
 
 
 MODELS = {
@@ -21,7 +83,7 @@ MODELS = {
         transform=ReorderedOneHot(
             alphabet="ACGT", dtype=np.float32, alphabet_axis=0, dummy_axis=1
         ),
-        scoring_fn=Diff(),
+        scoring_fn=diff,
     )
 }
 
@@ -32,6 +94,7 @@ def dataloader(
     variant_extractor = VariantSeqExtractor(fasta_file=fasta_file)
     for cv in VCF(vcf_file):
         variant = Variant.from_cyvcf(cv)
+        # Interval centered at the variant
         interval = Interval(
             variant.chrom, variant.pos - 1, variant.pos
         ).resize(sequence_length)
