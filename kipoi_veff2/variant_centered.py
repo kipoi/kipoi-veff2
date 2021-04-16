8c43867b06e8673a8bf8296cc947e1fafc2b964b
Haimasree <haimasree.de@gmail.com>
2021-04-16 17:20:50 +0200

    Adding scoring functions modification

--- kipoi_veff2/variant_centered.py
+++ kipoi_veff2/variant_centered.py
@@ -1,6 +1,8 @@
 import csv
 from dataclasses import dataclass
-from typing import Any, Callable, List, Iterator
+import itertools
+from pathlib import Path
+from typing import Any, Dict, List, Iterator, Callable, Union
 
 from cyvcf2 import VCF
 import numpy as np
@@ -9,11 +11,12 @@ from kipoiseq.dataclasses import Interval, Variant
 from kipoiseq.extractors import VariantSeqExtractor
 from kipoiseq.transforms import ReorderedOneHot
 
+MODELGROUPS = ["Basset", "DeepBind", "DeepSEA"]
+
 
 @dataclass
 class ModelConfig:
     model: str
-    scoring_fn: Callable[[List, List], List]
     required_sequence_length: int = None
     transform: Any = None
 
@@ -36,9 +39,15 @@ class ModelConfig:
                 dataloader_args = self.dataloader.default_args
                 self.transform = ReorderedOneHot(
                     alphabet="ACGT",
-                    dtype=dataloader_args["dtype"],
-                    alphabet_axis=dataloader_args["alphabet_axis"],
-                    dummy_axis=dataloader_args["dummy_axis"],
+                    dtype=dataloader_args["dtype"]
+                    if "dtype" in dataloader_args
+                    else None,
+                    alphabet_axis=dataloader_args["alphabet_axis"]
+                    if "alphabet_axis" in dataloader_args
+                    else 1,
+                    dummy_axis=dataloader_args["dummy_axis"]
+                    if "dummy_axis" in dataloader_args
+                    else None,
                 )
             else:
                 raise IOError("Only supporting sequence based models for now")
@@ -57,44 +66,34 @@ class ModelConfig:
         else:
             return self.required_sequence_length
 
-    def get_column_labels(self) -> List:
+    def get_column_labels(
+        self, list_of_scoring_fn: List[Dict[str, Callable[[Any, Any], List]]]
+    ) -> List:
         targets = self.model_description.schema.targets
         column_labels = targets.column_labels
         target_shape = targets.shape[0]
         if column_labels:
             if len(column_labels) == target_shape:
-                return [f"{self.model}/{c}" for c in column_labels]
+                return [
+                    f"{self.model}/{c}/{scoring_fn['name']}"
+                    for scoring_fn in list_of_scoring_fn
+                    for c in column_labels
+                ]
             else:
                 raise IOError(
                     "Something wrong with the model description - \
                         length of column names does not match target shape"
                 )
         else:
-            return [f"{self.model}/{num+1}" for num in range(target_shape)]
+            return [
+                f"{self.model}/{num+1}/{scoring_fn['name']}"
+                for scoring_fn in list_of_scoring_fn
+                for num in range(target_shape)
+            ]
 
 
-def diff(ref_pred: List, alt_pred: List) -> List:
-    return list(alt_pred - ref_pred)
-
-
-MODELS = {
-    "Basset": ModelConfig(
-        model="Basset",
-        scoring_fn=diff,
-    ),
-    "DeepSEA/beluga": ModelConfig(
-        model="DeepSEA/beluga",
-        scoring_fn=diff,
-    ),
-    "DeepSEA/predict": ModelConfig(
-        model="DeepSEA/predict",
-        scoring_fn=diff,
-    ),
-    "DeepSEA/variantEffects": ModelConfig(
-        model="DeepSEA/variantEffects",
-        scoring_fn=diff,
-    ),
-}
+def get_model_config(model_name: str) -> ModelConfig:
+    return ModelConfig(model=model_name)
 
 
 def dataloader(
@@ -117,12 +116,18 @@ def dataloader(
 
 
 def score_variants(
-    model_config: ModelConfig, vcf_file: str, fasta_file: str, output_file: str
+    model_config: ModelConfig,
+    vcf_file: str,
+    fasta_file: str,
+    output_file: Union[str, Path],
+    list_of_scoring_fn: List[Dict[str, Callable[[Any, Any], List]]],
 ) -> None:
     kipoi_model = kipoi.get_model(model_config.model)
     sequence_length = model_config.get_required_sequence_length()
     transform = model_config.get_transform()
-    column_labels = model_config.get_column_labels()
+    column_labels = model_config.get_column_labels(
+        list_of_scoring_fn=list_of_scoring_fn
+    )
     with open(output_file, "w") as output_tsv:
         tsv_writer = csv.writer(output_tsv, delimiter="\t")
         tsv_writer.writerow(
@@ -137,7 +142,11 @@ def score_variants(
             alt_prediction = kipoi_model.predict_on_batch(
                 transform(alt)[np.newaxis]
             )[0]
-            score = model_config.scoring_fn(ref_prediction, alt_prediction)
+            scores = [
+                scoring_fn["func"](ref_prediction, alt_prediction)
+                for scoring_fn in list_of_scoring_fn
+            ]
+            scores = list(itertools.chain.from_iterable(scores))
             tsv_writer.writerow(
                 [
                     variant.chrom,
@@ -146,5 +155,5 @@ def score_variants(
                     variant.ref,
                     variant.alt,
                 ]
-                + score
+                + scores
             )
--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -2,11 +2,13 @@ import csv
 from pathlib import Path
 
 from kipoi_veff2 import variant_centered
+from kipoi_veff2 import scoring_functions
+
 import pytest
 
 
 def test_modelconfig():
-    test_model_config = variant_centered.MODELS["DeepSEA/predict"]
+    test_model_config = variant_centered.get_model_config("DeepSEA/predict")
     assert test_model_config.get_model() == "DeepSEA/predict"
     assert test_model_config.get_required_sequence_length() == 1000
     assert (
@@ -21,30 +23,50 @@ def test_modelconfig():
 @pytest.mark.parametrize(
     "model_name, header_name, number_of_headers",
     [
-        ("Basset", "Basset/PANC", 169),
-        ("DeepSEA/beluga", "DeepSEA/beluga/Osteoblasts_H4K20me1_None", 2007),
-        ("DeepSEA/predict", "DeepSEA/predict/Osteoblasts_H3K9me3_None", 924),
+        ("Basset", "Basset/PANC/diff", 169),
+        (
+            "DeepBind/Homo_sapiens/RBP/D00084.001_RNAcompete_A1CF",
+            "DeepBind/Homo_sapiens/RBP/D00084.001_RNAcompete_A1CF/1/diff",
+            6,
+        ),
+        (
+            "DeepSEA/beluga",
+            "DeepSEA/beluga/Osteoblasts_H4K20me1_None/diff",
+            2007,
+        ),
+        (
+            "DeepSEA/predict",
+            "DeepSEA/predict/Osteoblasts_H3K9me3_None/diff",
+            924,
+        ),
         (
             "DeepSEA/variantEffects",
-            "DeepSEA/variantEffects/Osteoblasts_H3K9me3_None",
+            "DeepSEA/variantEffects/Osteoblasts_H3K9me3_None/diff",
             924,
         ),
     ],
 )
-def test_scoring(model_name, header_name, number_of_headers):
-    test_model_config = variant_centered.MODELS[model_name]
+def test_scoring_single_scoring_function(
+    model_name, header_name, number_of_headers
+):
+    test_model_config = variant_centered.get_model_config(model_name)
+    assert test_model_config.get_model() == model_name
     test_dir = Path(__file__).resolve().parent
     vcf_file = str(test_dir / "data" / "singlevariant.vcf")
     fasta_file = str(test_dir / "data" / "hg38_chr22.fa")
     model_config = test_model_config
-    output_file = str(test_dir / "data" / "out.tsv")
+    output_file = Path(
+        test_dir / "data" / f"out.{model_name.replace('/', '_')}.tsv"
+    )
+
     variant_centered.score_variants(
         model_config=model_config,
         vcf_file=vcf_file,
         fasta_file=fasta_file,
         output_file=output_file,
+        list_of_scoring_fn=[{"name": "diff", "func": scoring_functions.diff}],
     )
-    assert Path(output_file).exists()
+    assert output_file.exists()
     with open(output_file, "r") as output_file_handle:
         tsv_reader = csv.reader(output_file_handle, delimiter="\t")
         header = next(tsv_reader)
@@ -53,3 +75,70 @@ def test_scoring(model_name, header_name, number_of_headers):
         row = next(tsv_reader)
         assert row[2] == ""
         assert len(row) == number_of_headers
+    output_file.unlink()
+
+
+@pytest.mark.parametrize(
+    "model_name, diff_header_name, logit_header_name, number_of_headers",
+    [
+        ("Basset", "Basset/8988T/diff", "Basset/PANC/logit", 333),
+        (
+            "DeepBind/Homo_sapiens/RBP/D00084.001_RNAcompete_A1CF",
+            "DeepBind/Homo_sapiens/RBP/D00084.001_RNAcompete_A1CF/1/diff",
+            "DeepBind/Homo_sapiens/RBP/D00084.001_RNAcompete_A1CF/1/logit",
+            7,
+        ),
+        (
+            "DeepSEA/beluga",
+            "DeepSEA/beluga/8988T_DNase_None/diff",
+            "DeepSEA/beluga/Osteoblasts_H4K20me1_None/logit",
+            4009,
+        ),
+        (
+            "DeepSEA/predict",
+            "DeepSEA/predict/8988T_DNase_None/diff",
+            "DeepSEA/predict/Osteoblasts_H3K9me3_None/logit",
+            1843,
+        ),
+        (
+            "DeepSEA/variantEffects",
+            "DeepSEA/variantEffects/8988T_DNase_None/diff",
+            "DeepSEA/variantEffects/Osteoblasts_H3K9me3_None/logit",
+            1843,
+        ),
+    ],
+)
+def test_scoring_multiple_scoring_functions(
+    model_name, diff_header_name, logit_header_name, number_of_headers
+):
+    test_model_config = variant_centered.get_model_config(model_name)
+    assert test_model_config.get_model() == model_name
+    test_dir = Path(__file__).resolve().parent
+    vcf_file = str(test_dir / "data" / "singlevariant.vcf")
+    fasta_file = str(test_dir / "data" / "hg38_chr22.fa")
+    model_config = test_model_config
+    output_file = Path(
+        test_dir / "data" / f"out.{model_name.replace('/', '_')}.tsv"
+    )
+
+    variant_centered.score_variants(
+        model_config=model_config,
+        vcf_file=vcf_file,
+        fasta_file=fasta_file,
+        output_file=output_file,
+        list_of_scoring_fn=[
+            {"name": "diff", "func": scoring_functions.diff},
+            {"name": "logit", "func": scoring_functions.logit},
+        ],
+    )
+    assert output_file.exists()
+    with open(output_file, "r") as output_file_handle:
+        tsv_reader = csv.reader(output_file_handle, delimiter="\t")
+        header = next(tsv_reader)
+        assert len(header) == number_of_headers
+        assert header[5] == diff_header_name
+        assert header[number_of_headers - 1] == logit_header_name
+        row = next(tsv_reader)
+        assert row[2] == ""
+        assert len(row) == number_of_headers
+    output_file.unlink()
