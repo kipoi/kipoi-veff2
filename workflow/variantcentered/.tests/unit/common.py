8b063771a8da2df9cd9c9fb798e7f5c590024209
Haimasree <haimasree.de@gmail.com>
2021-05-07 13:14:57 +0200

    working version added

---
+++ workflow/variantcentered/.tests/unit/common.py
@@ -0,0 +1,54 @@
+"""
+Common code for unit testing of rules generated with Snakemake 6.2.1.
+"""
+
+from pathlib import Path
+import subprocess as sp
+import os
+
+import pandas as pd
+
+
+class OutputChecker:
+    def __init__(self, data_path, expected_path, workdir):
+        self.data_path = data_path
+        self.expected_path = expected_path
+        self.workdir = workdir
+
+    def check(self):
+        input_files = set(
+            (Path(path) / f).relative_to(self.data_path)
+            for path, subdirs, files in os.walk(self.data_path)
+            for f in files
+        )
+        expected_files = set(
+            (Path(path) / f).relative_to(self.expected_path)
+            for path, subdirs, files in os.walk(self.expected_path)
+            for f in files
+        )
+        unexpected_files = set()
+        for path, subdirs, files in os.walk(self.workdir):
+            for f in files:
+                f = (Path(path) / f).relative_to(self.workdir)
+                if str(f).startswith(".snakemake"):
+                    continue
+                if f in expected_files:
+                    self.compare_files(
+                        self.workdir / f, self.expected_path / f
+                    )
+                elif f in input_files:
+                    # ignore input files
+                    pass
+                else:
+                    unexpected_files.add(f)
+        if unexpected_files:
+            raise ValueError(
+                "Unexpected files:\n{}".format(
+                    "\n".join(sorted(map(str, unexpected_files)))
+                )
+            )
+
+    def compare_files(self, generated_file, expected_file):
+        df_generated = pd.read_csv(generated_file, sep="\t")
+        df_expected = pd.read_csv(expected_file, sep="\t")
+        pd._testing.assert_frame_equal(df_expected, df_generated)
