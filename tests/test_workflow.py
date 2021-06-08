c54a73239d4fbdc9bf77774f77c719b527552ac7
Haimasree <haimasree.de@gmail.com>
2021-06-08 13:21:16 +0200

    Trying to fix test

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 
 def test_snakemake_workflow():
-    assert Path("tests/output.merged.tsv").exists()
-    df_merged = pd.read_csv("tests/output.merged.tsv", sep="\t")
+    assert Path("examples/output.merged.tsv").exists()
+    df_merged = pd.read_csv("examples/output.merged.tsv", sep="\t")
     assert len(df_merged) == 3075
     assert len(df_merged.columns) == 389
