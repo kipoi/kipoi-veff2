0eef5992a692b8a07ae735b1dd193d33038c7cc7
Haimasree <haimasree.de@gmail.com>
2021-07-12 14:09:59 +0200

    More tests added

--- tests/test_snakefilegenerator_cli.py
+++ tests/test_snakefilegenerator_cli.py
@@ -43,3 +43,105 @@ def test_cli_correct_use(runner, tmp_path):
         lines = snakefile_handle.readlines()
 
     assert lines[6] == "models = ['DeepSEA/predict', 'Basset']\n"
+
+
+def test_cli_correct_use_different_flag(runner, tmp_path):
+    test_dir = Path(__file__).resolve().parent
+    output_dir = tmp_path / "workflow"
+    output_dir.mkdir()
+    result = runner.invoke(
+        snakefilegenerator.generate_snakefiles,
+        [
+            str(test_dir / "data" / "general" / "test.vcf"),
+            str(test_dir / "data" / "general" / "hg38_chr22.fa"),
+            str(output_dir),
+            "--model-groups",
+            "DeepSEA",
+            "--model-groups",
+            "Basset",
+            "--number-of-shards",
+            "2",
+        ],
+    )
+    assert result.exit_code == 0
+    assert (output_dir / "Snakefile.0").exists()
+    assert (output_dir / "Snakefile.1").exists()
+    with open(output_dir / "Snakefile.0", "r") as snakefile_handle:
+        lines = snakefile_handle.readlines()
+
+    assert (
+        lines[6] == "models = ['DeepSEA/variantEffects', 'DeepSEA/beluga']\n"
+    )
+
+    with open(output_dir / "Snakefile.1", "r") as snakefile_handle:
+        lines = snakefile_handle.readlines()
+
+    assert lines[6] == "models = ['DeepSEA/predict', 'Basset']\n"
+
+
+def test_cli_input_vcf_does_not_exist(runner):
+    result = runner.invoke(
+        snakefilegenerator.generate_snakefiles,
+        ["in.vcf", "in.fa", "out_dir", "-mg", "DeepSEA", "-n", "2"],
+    )
+    assert result.exit_code == 2
+    assert "Error: Invalid value for 'INPUT_VCF'" in result.output
+
+
+def test_cli_input_fasta_does_not_exist(runner):
+    test_dir = Path(__file__).resolve().parent
+    result = runner.invoke(
+        snakefilegenerator.generate_snakefiles,
+        [
+            str(test_dir / "data" / "general" / "test.vcf"),
+            "in.fa",
+            "out_dir",
+            "-mg",
+            "DeepSEA",
+            "-n",
+            "2",
+        ],
+    )
+    assert result.exit_code == 2
+    assert "Error: Invalid value for 'INPUT_FASTA'" in result.output
+
+
+def test_cli_output_dir_does_not_exist(runner, tmp_path):
+    test_dir = Path(__file__).resolve().parent
+    output_dir = tmp_path / "workflow"
+    result = runner.invoke(
+        snakefilegenerator.generate_snakefiles,
+        [
+            str(test_dir / "data" / "general" / "test.vcf"),
+            str(test_dir / "data" / "general" / "hg38_chr22.fa"),
+            str(output_dir),
+            "-mg",
+            "DeepSEA",
+            "-mg",
+            "Basset",
+            "-n",
+            "2",
+        ],
+    )
+    assert result.exit_code == 2
+    assert "does not exist" in result.output
+
+
+def test_cli_wrong_model(runner, tmp_path):
+    test_dir = Path(__file__).resolve().parent
+    output_dir = tmp_path / "workflow"
+    output_dir.mkdir()
+    result = runner.invoke(
+        snakefilegenerator.generate_snakefiles,
+        [
+            str(test_dir / "data" / "general" / "test.vcf"),
+            str(test_dir / "data" / "general" / "hg38_chr22.fa"),
+            str(output_dir),
+            "-mg",
+            "Deepsea",
+            "-n",
+            "2",
+        ],
+    )
+    assert result.exit_code == 2
+    assert "Not adding Deepsea as it is not supported yet" in result.output
