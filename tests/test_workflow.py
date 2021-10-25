b3f1b07406071fa2a09196ae44e2abf8cebec09c
Haimasree <haimasree.de@gmail.com>
2021-10-25 11:35:33 +0200

    Fixing another value

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -6,5 +6,5 @@ import pandas as pd
 def test_snakemake_workflow():
     assert Path("examples/output.merged.tsv").exists()
     df_merged = pd.read_csv("examples/output.merged.tsv", sep="\t")
-    assert len(df_merged) == 3075
+    assert len(df_merged) == 3074
     assert len(df_merged.columns) == 389
